1f8b7aa2877f43fb7343c53f4b25a74ff3317da4
“poncedavid” <Da.ponce@duocuc.cl>
2024-02-27 15:03:46 -0300

    Actualización de dependencias y rutas de autenticación

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,76 +1,68 @@
-import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-import { createAccessToken } from '../libs/jwt.js';
-
-
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
 
-        const passwordHash = await bcrypt.hash(password, 10) 
-
-
-        const newUser = new User({
-            username,
-            email,
-            password: passwordHash
-        });
-        const userSaved = await newUser.save(); // Guardar en la base de datos
-        const token = await createAccessToken({ id: userSaved._id }); // Crear token
-        res.cookie("token", token);
-        res.json({
-            //Para no mostrar el password 
-            // Parametros que se van a mostrar en el front
-            _id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt
-        
-        });
-
-
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+    const userSaved = await newUser.save(); // Guardar en la base de datos
+    const token = await createAccessToken({ id: userSaved._id }); // Crear token
+    res.cookie("token", token);
+    res.json({
+      //Para no mostrar el password
+      // Parametros que se van a mostrar en el front
+      _id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound)
+      return res.status(400).json({ message: "Usuario no encontrado" });
 
-        const userFound = await User.findOne({email})
-        if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" });
+    const isMatch = await bcrypt.compare(password, userFound.password);
 
-        const isMatch = await bcrypt.compare(password, userFound.password) 
+    if (!isMatch)
+      return res.status(400).json({ message: "Contraseña incorrecta" });
 
-        if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" });
+    // no es recomendado enviar tantos mensajes de usuario y contraseña incorrecta por seguridad. La persona que intente hackear la cuenta puede saber si el usuario existe o no.
+    // Se recomienda enviar un mensaje generico como "Usuario o contraseña incorrecta"
 
-        // no es recomendado enviar tantos mensajes de usuario y contraseña incorrecta por seguridad. La persona que intente hackear la cuenta puede saber si el usuario existe o no.
-        // Se recomienda enviar un mensaje generico como "Usuario o contraseña incorrecta"
+    const token = await createAccessToken({ id: userFound._id }); // Crear token
+    res.cookie("token", token);
+    res.json({
+      //Para no mostrar el password
+      // Parametros que se van a mostrar en el front
+      _id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-        const token = await createAccessToken({ id: userFound._id }); // Crear token 
-        res.cookie("token", token);
-        res.json({
-            //Para no mostrar el password 
-            // Parametros que se van a mostrar en el front
-            _id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt
-        
-        });
-
-
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+export const logout = (req, res) => {
+    res.cookie("token", "")
 }
-
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, logout, register } from "../controllers/auth.controller.js";
 
 
 const router = Router();
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/logout", logout)
 
 export default router;
